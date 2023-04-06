7321e43b1fd63bb65c15015b538a18a382d2f0a4
artitur05 <artitur05@gmail.com>
2023-04-06 19:58:52 +0700

    создание ветки для конфликта

--- index.php
+++ index.php
@@ -2,15 +2,12 @@
 index master master
 to do homework i need
 to make some conflict
-
 and if i would succseed
 i would start 
 a code war
 to clear out the commits
-
 also i wanna try rebase 
 by the way
 commit slave race
-
 this is 8-th commit
 and this is 9-th
